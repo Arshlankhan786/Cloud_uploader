fc878950e29979af9abb10b4b84fee50f7990786
Arshlankhan786 <141414515+Arshlankhan786@users.noreply.github.com>
2023-11-30 19:01:11 +0530

    Update file_download.py

--- bot/helpers/file_download.py
+++ bot/helpers/file_download.py
@@ -9,7 +9,7 @@ logger = LOGGER(__name__)
 async def download_media(client: CloudBot, message: CallbackQuery, ) -> str:
     user_message = await client.edit_message_text(
         chat_id=message.from_user.id,
-        message_id=message.message.message_id,
+        message_id=message.message.id,
         text="processing your request...",
     )
     try:
