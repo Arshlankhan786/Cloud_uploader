fb38edb1f13a7d60a415cf3da3974d42e67aab9e
Arshlankhan786 <141414515+Arshlankhan786@users.noreply.github.com>
2023-12-02 12:53:16 +0530

    Update help.py

--- bot/plugins/commands/help.py
+++ bot/plugins/commands/help.py
@@ -6,6 +6,6 @@ from bot import HELP, SOURCE
 async def help_message(client, message):
     await client.send_message(
         chat_id=message.chat.id,
-        text=f"Hey {message.from_user.first_name},{HELP}{SOURCE}",
+        text=f"Hey {message.from_user.first_name},{HELP}",
         reply_to_message_id=message.id,
     )
