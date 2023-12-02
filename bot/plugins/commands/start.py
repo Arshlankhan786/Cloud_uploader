300afa2b54aae3e72d167ea674ee638bf69da4ff
Arshlankhan786 <141414515+Arshlankhan786@users.noreply.github.com>
2023-12-02 12:53:31 +0530

    Update start.py

--- bot/plugins/commands/start.py
+++ bot/plugins/commands/start.py
@@ -6,6 +6,6 @@ from bot import START, SOURCE
 async def start_message(client, message):
     await client.send_message(
         chat_id=message.chat.id,
-        text=f"Hey {message.from_user.first_name},{START}{SOURCE}",
+        text=f"Hey {message.from_user.first_name},{START}",
         reply_to_message_id=message.id,
     )
